d10b8607ecc03cf6f15a2261e7e20b0f435231ed
seba lepe <slepe@uc.cl>
2021-07-03 17:07:33 -0400

    algo

--- Sites/usuarios/validacion_ingreso.php
+++ Sites/usuarios/validacion_ingreso.php
@@ -29,7 +29,7 @@
     <p> contraseña incorrecta </p>
     <?php else: ?>
         <?php 
-        $_SESSION['current_user'] =  "$usuario[0]";
+        $_SESSION['current_user'] =  $usuario[0];
         $_SESSION['current_password'] =  $usuario[0];
         ?>
         <meta http-equiv="refresh" content="0;url=../usuarios/perfil.php">
