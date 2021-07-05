e2c9130e312237c5f5b93c5ab63e6056ead3b26e
bmaldonadoc4 <bmaldonadoc4@uc.cl>
2021-07-05 15:11:39 -0400

    a

---
+++ data/data_loader2.php
@@ -0,0 +1,65 @@
+<?php
+function send_base_62($valor)
+{
+    $conn = @pg_connect("dbname=grupo62e3 user=grupo62 password=grupo62");
+    $envio = @pg_query($conn, strval($valor));
+    return $envio;
+}
+?>
+<?php
+function send_base_87($valor)
+{
+    $conn = @pg_connect("dbname=grupo87e3 user=grupo87 password=grupo87");
+    $envio = @pg_query($conn, strval($valor));
+    return $envio;
+}
+?>
+<?php # Migrar personal
+    require ("../Sites/config/conexion.php");
+
+    #Se construye la consulta como un string
+    $query = "SELECT * FROM direcciones;";
+
+    #Se prepara y ejecuta la consulta. Se obtienen TODOS los resultados
+    $result = $db->prepare($query);
+    $result->execute();
+    $Direcciones = $result->fetchAll();
+    $direc = array();
+    foreach ($Direcciones as $d) {
+        array_push($direc, $d[0]);
+        array_push($direc, $d[1]);
+    }
+
+
+    require ("../Sites/config/conexion.php");
+
+    #Se construye la consulta como un string
+    $query = "SELECT * FROM personal;";
+
+    #Se prepara y ejecuta la consulta. Se obtienen TODOS los resultados
+    $result = $db->prepare($query);
+    $result->execute();
+    $Personal = $result->fetchAll();
+    $contador = 345;
+    foreach ($Personal as $p) {
+        $dato1 = $contador; #ID
+        $contador = $contador + 1; 
+        $dato2 = $p[1]; # rut
+        $dato3 = $p[2]; # nombre
+        $dato4 = substr($p[3], 0, 1); # sexo
+        $dato5 = $p[4]; # edad
+        $dato6 = $p[5]; # direccion
+        for ($i; $i < count($direc); $i++){
+            if (strval($direct[$i]) == $dato6) {
+                $dato6 = intval($direct[$i-1]);
+            }
+        }
+        $dato7 = $p[6]; # contraseña
+        $dato8 = $p[7]; # es_jefe
+        #send_base_87("INSERT INTO usuarios VALUES($dato2,'',$dato3,$dato5,$dato4,$dato6,$dato1,$dato7,$dato8,'');");
+
+        $query = "INSERT INTO usuarios(ru,direcciones,nombre,edad,sexo,direccion,id,contraseña,es_jefe,carrito) VALUES($dato2,'',$dato3,$dato5,$dato4,$dato6,$dato1,$dato7,$dato8,'');";
+        $result = $db2 ->prepare($query);
+        $result ->execute();
+    }
+?>
