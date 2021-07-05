1949cf8763ba79d8b516959042f965dcc6381558
bmaldonadoc4 <bmaldonadoc4@uc.cl>
2021-07-05 16:00:26 -0400

    a

--- data/data_loader.php
+++ data/data_loader.php
@@ -14,75 +14,6 @@ function send_base_87($valor)
     return $envio;
 }
 ?>
-<?php
-
-#-------------------------------https://stackoverflow.com/questions/4356289/php-random-string-generator-----
-function generateRandomString($length = 30) {
-    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    $charactersLength = strlen($characters);
-    $randomString = '';
-    for ($i = 0; $i < $length; $i++) {
-        $randomString .= $characters[rand(0, $charactersLength - 1)];
-    }
-    return $randomString;
-}
-#-----------------------------------------------------------------------------------------------------------
-?>
-
-<?php # Cargar acontraseñas
-    #Llama a conexión, crea el objeto PDO y obtiene la variable $db
-    require ("../Sites/config/conexion.php");
-
-    #Se construye la consulta como un string
-    $query = "SELECT rut FROM personal;";
-
-    #Se prepara y ejecuta la consulta. Se obtienen TODOS los resultados
-    $result = $db->prepare($query);
-    $result->execute();
-    $Ruts = $result->fetchAll();
-    
-    foreach ($Ruts as $r) {
-        $dato = generateRandomString(); # Contraseña
-        send_base_62("UPDATE personal SET contraseña='". $dato ."' WHERE rut='" . $r[0] . "';");
-    }
-
-?>
-
-
-<?php 
-    require ("../Sites/config/conexion.php");
-
-    #Se construye la consulta como un string
-    $query = "SELECT rut FROM personal;";
-
-    #Se prepara y ejecuta la consulta. Se obtienen TODOS los resultados
-    $result = $db->prepare($query);
-    $result->execute();
-    $Personal = $result->fetchAll();
-    foreach ($Personal as $p) {
-        send_base_62("UPDATE personal SET es_jefe=0 WHERE rut='" . $p[0] . "';");
-    }
-    #Llama a conexión, crea el objeto PDO y obtiene la variable $db
-    require ("../Sites/config/conexion.php");
-
-    #Se construye la consulta como un string
-    $query = "SELECT clasificacion,rut FROM p_clasificados;";
-
-    #Se prepara y ejecuta la consulta. Se obtienen TODOS los resultados
-    $result = $db->prepare($query);
-    $result->execute();
-    $Clasificados = $result->fetchAll();
-
-    foreach ($Clasificados as $c) {
-        if (strval($c[0]) == "administracion"){
-            send_base_62("UPDATE personal SET es_jefe=1 WHERE rut='" . $c[1] . "';");
-        } else {
-            send_base_62("UPDATE personal SET es_jefe=0 WHERE rut='" . $c[1] . "';");
-        }
-    }
-   
-?>
-
 <?php # Migrar personal
     require ("../Sites/config/conexion.php");
 
@@ -99,7 +30,6 @@ function generateRandomString($length = 30) {
         array_push($direc, $d[1]);
     }
 
-
     require ("../Sites/config/conexion.php");
 
     #Se construye la consulta como un string
@@ -109,7 +39,7 @@ function generateRandomString($length = 30) {
     $result = $db->prepare($query);
     $result->execute();
     $Personal = $result->fetchAll();
-    $contador = 345;
+    $contador = 365;
     foreach ($Personal as $p) {
         $dato1 = $contador; #ID
         $contador = $contador + 1; 
@@ -119,17 +49,14 @@ function generateRandomString($length = 30) {
         $dato5 = $p[4]; # edad
         $dato6 = $p[5]; # direccion
         for ($i; $i < count($direc); $i++){
-            if (strval($direct[$i]) == $dato6) {
-                $dato6 = intval($direct[$i-1])
+            if (strval($direc[$i]) == strval($dato6)) {
+                $dato6 = intval($direc[$i-1]);
             }
         }
         $dato7 = $p[6]; # contraseña
         $dato8 = $p[7]; # es_jefe
-        #send_base_87("INSERT INTO usuarios VALUES($dato2,'',$dato3,$dato5,$dato4,$dato6,$dato1,$dato7,$dato8,'');");
-
-        $query = "INSERT INTO usuarios VALUES($dato2,'',$dato3,$dato5,$dato4,$dato6,$dato1,$dato7,$dato8,'');";
+        $query = "INSERT INTO usuarios(rut,direcciones,nombre,edad,sexo,direccion,id,contraseña,es_jefe,carrito) VALUES('$dato2','','$dato3',$dato5,'$dato4',$dato6,$dato1,'$dato7',$dato8,'');";
         $result = $db2 ->prepare($query);
         $result ->execute();
     }
 ?>
-
