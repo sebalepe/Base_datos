933124a30f55123b58d692db0854d98f267cfc57
seba lepe <slepe@uc.cl>
2021-07-04 20:00:35 -0400

    algo

--- Sites/Tiendas/show.php
+++ Sites/Tiendas/show.php
@@ -2,30 +2,78 @@
 
 <body>
 <?php
-  #Llama a conexión, crea el objeto PDO y obtiene la variable $db
+  
   require("../config/conexion.php");
 
-  #Se obtiene el valor del input del usuario
+  
   $id = $_POST["id"];
 
-  echo $id;
+  $query = "SELECT * from tiendas where id = '$id' ;";
 
-  #Se construye la consulta como un string
+ 
+  $result = $db2 -> prepare($query);
+  $result -> execute();
+  $Tiendas = $result -> fetchAll(); 
+  $Tienda = $Tiendas[0]; 
+
+
+  echo "
+    <div align='center'>
+        <p class='title is-3'>
+            Bienvenido a $Tienda[2] 
+        </p>
+
+        <p class='subtitle is-5'>
+          20% de descuento con CMR en todos los productos
+        </p>
+    </div"
+
+
+?>
+
+<div class="modal">
+  <div class="modal-background"></div>
+  <div class="modal-card">
+    <header class="modal-card-head">
+      <p class="modal-card-title">Test</p>
+      <button class="delete" aria-label="close"></button>
+    </header>
+    <section class="modal-card-body">
+     WENA
+    </section>
+    <footer class="modal-card-foot">
+      <button class="button">Volver</button>
+    </footer>
+  </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+  <!--
  	$query = "SELECT * from tiendas where id = '$id' ;";
 
-  #Se prepara y ejecuta la consulta. Se obtienen TODOS los resultados
+ 
 	$result = $db2 -> prepare($query);
 	$result -> execute();
-	$Tiendas = $result -> fetchAll();
-  $Tienda = $Tiendas[0];
+	$Tiendas = $result -> fetchAll(); 
+  $Tienda = $Tiendas[0]; 
   
 
   foreach ($Tienda as $value) {
      echo "<p> $value </p>";
   }
 
-  ?>
-
+?>
+-->
 
 
 <?php include('../templates/footer.html'); ?>
