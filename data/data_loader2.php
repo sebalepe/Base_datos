101914303ccccae015d52ba9e5507f64a2bb0e5e
bmaldonadoc4 <bmaldonadoc4@uc.cl>
2021-07-05 15:38:38 -0400

    a

--- data/data_loader2.php
+++ data/data_loader2.php
@@ -56,7 +56,6 @@ function send_base_87($valor)
         }
         $dato7 = $p[6]; # contraseña
         $dato8 = $p[7]; # es_jefe
-        #send_base_87("INSERT INTO usuarios VALUES($dato2,'',$dato3,$dato5,$dato4,$dato6,$dato1,$dato7,$dato8,'');");
 
         $query = "INSERT INTO usuarios(rut,direcciones,nombre,edad,sexo,direccion,id,contraseña,es_jefe,carrito) VALUES($dato2,'',$dato3,$dato5,$dato4,$dato6,$dato1,$dato7,$dato8,'');";
         echo $query;
