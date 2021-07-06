fe13b52123553b68325e72d78f1a0d774e631382
feliperojas123 <feliperojas.cj@gmail.com>
2021-07-05 22:39:04 -0400

    a

--- Sites/usuarios/perfil.php
+++ Sites/usuarios/perfil.php
@@ -135,6 +135,8 @@
                         $unidad = $result -> fetchAll();
                         $uni = $unidad[0][0];
                         $direc_boss = $unidad[0][1];
+                        echo $uni;
+
 
                         $query = "SELECT nombre FROM personal, p_clasificados where personal.rut = p_clasificados.rut and unidad = '$uni' and p_clasificados.rut <> '$rut';";
 
