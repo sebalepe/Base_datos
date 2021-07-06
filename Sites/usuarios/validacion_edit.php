96d5137d92a25e24c5d6086f44824a5c33bdce39
seba lepe <slepe@uc.cl>
2021-07-06 00:02:31 -0400

    prueba

---
+++ Sites/usuarios/validacion_edit.php
@@ -0,0 +1,79 @@
+<?php 
+    session_start();
+    $rut = $_SESSION['current_user'];
+    $contraseña = $_SESSION['current_password'];
+?>
+
+<?php include('../templates/header.html');  ?>
+
+<?php 
+require("../config/conexion.php");
+
+
+$old_contraseña = $_POST['old_contraseña'];
+$new_constraseña = $_POST['new_constraseña'];
+$new_direccion = $_POST['direccion'];
+$new_comuna = $_POST['comuna'];
+
+
+echo "
+
+<section class='hero notification is-fullheight'>
+	<div class='tile is-ancestor'>
+		<div class='tile is-child box'>
+";
+
+if (!empty($old_contraseña) and !empty($new_contraseña)){
+
+	if ($old_contraseña == $contraseña){
+		$query = "UPDATE usuarios 
+				 set contraseña = '$new_contraseña'
+				 where rut = '$rut';";
+		$result = $db2 -> prepare($query);
+		$result -> execute();
+		echo "<p> Contraseña actual actualizada a <p> "
+	else{
+		echo "<p> Contraseña actual no concuerda <p>"
+	}
+	}
+}
+
+if (!empty($new_direccion)){
+
+	$query = "SELECT direccion from usuarios where rut = '$rut' ;";
+	$result = $db2 -> prepare($query);
+	$result -> execute();
+	$id_direccion = $result[0][0];
+
+	$query = "UPDATE direcciones
+			  set direccion = '$new_direccion'
+			  where id = '$id_direccion';";
+	$result = $db2 -> prepare($query);
+	$result -> execute();
+	echo "<p> Direccion actual actualizada a $new_direccion <p> "
+}
+
+if (!empty($new_comuna)){
+
+	$query = "SELECT direccion from usuarios where rut = '$rut' ;";
+	$result = $db2 -> prepare($query);
+	$result -> execute();
+	$id_direccion = $result[0][0];
+
+	$query = "UPDATE direcciones
+			  set comuna = '$new_comuna'
+			  where id = '$id_direccion';";
+	$result = $db2 -> prepare($query);
+	$result -> execute();
+	echo "<p> Contraseña actual actualizada a $new_comuna <p> "
+}
+
+
+echo "
+		<div class='tile is-child box'>
+	<div class='tile is-ancestor'>
+<section class='hero notification is-fullheight'>
+";
+?>
+
+<?php include('../templates/footer.html'); ?>
