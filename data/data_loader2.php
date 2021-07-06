b39f3e9ae6306e1f2307575d26a7b9f720a898c9
bmaldonadoc4 <bmaldonadoc4@uc.cl>
2021-07-05 20:45:33 -0400

    a

--- data/data_loader2.php
+++ data/data_loader2.php
@@ -27,7 +27,7 @@ function generateRandomNum($length = 3) {
     
     foreach ($Comida as $c) {
         $dato = intval(generateRandomNum()); # Contraseña
-        $query = "INSERT INTO comestibles(cantidad) VALUES($dato);";
+        $query = "UPDATE comestibles(cantidad) VALUES($dato);";
         $result = $db2 ->prepare($query);
         echo $dato;
         echo $query;
