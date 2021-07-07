5fae4d09661a2f1cb50140ce1bbfc0b877950453
seba lepe <slepe@uc.cl>
2021-07-06 22:36:41 -0400

    algo

--- Sites/Tiendas/show.php
+++ Sites/Tiendas/show.php
@@ -359,15 +359,15 @@ echo "</p>
           $cantidad = $_POST['cantidad'];
           $compra = array_replace($_SESSION['compra'], array(4 => intval($cantidad)));
           echo "<p> procedemos a hacer tu compra de $cantidad productos </p> ";
-          $query = "SELECT generar_compra($compra[0], $compra[1], $compra[2], $compra[3], $compra[4], $compra[5], $compra[6]);";
+          $query = "SELECT generar_compra('1', $compra[1], $compra[2], $compra[3], $compra[4], $compra[5], $compra[6]);";
           #$query = "SELECT generar_compra(". intval($id_compra) .", ". intval($id_user) .", ". intval($id) .", ". intval($id_producto) .", ". intval($cantidad) .", ". intval($tipo) .", ". intval($max_cant) .");";
           echo $query;
           
           #$query = "SELECT generar_compra(2001, 2001, 2001, 2001, 2001, 2001, 2001);";
           try { 
-          $result = $db2 -> prepare($query);
-          $result -> execute();
-          $_SESSION['compra'] = array();
+            $result = $db2 -> prepare($query);
+            $result -> execute();
+            $_SESSION['compra'] = array();
           } 
           catch (Exception $e) {
                 echo 'Excepción capturada: ',  $e->getMessage(), "\n";
