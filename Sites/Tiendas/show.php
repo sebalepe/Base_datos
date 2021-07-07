128c19679b179c59ff57cb65c125643fcd7eb4cd
bmaldonadoc4 <bmaldonadoc4@uc.cl>
2021-07-07 00:02:00 -0400

    a

--- Sites/Tiendas/show.php
+++ Sites/Tiendas/show.php
@@ -367,15 +367,13 @@ echo "</p>
           $cantidad = $_POST['cantidad'];
           $compra = array_replace($_SESSION['compra'], array(4 => intval($cantidad)));
           echo "<p> procedemos a hacer tu compra de $cantidad productos </p> ";
-          $query = "SELECT generar_compra($compra[0],$compra[1],$compra[2],$compra[3],$compra[4],$compra[5],$compra[6]);";
+          $query = "SELECT generar_compra(". $compra[0] .", ". $compra[1] .", ". $compra[2] .", ". $compra[3] .", ". $compra[4] .", ". $compra[5] .", ". $compra[6] .");";
           #$query = "SELECT generar_compra(". intval($id_compra) .", ". intval($id_user) .", ". intval($id) .", ". intval($id_producto) .", ". intval($cantidad) .", ". intval($tipo) .", ". intval($max_cant) .");";
 
           
           #$query = "SELECT generar_compra(2001, 2001, 2001, 2001, 2001, 2001, 2001);";
           $result = $db2 -> prepare($query);
           $result -> execute();   
-          $hola = $result -> fetchAll();
-          echo count($hola);
           $_SESSION['compra'] = array();
         
       }
